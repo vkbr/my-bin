e1f132838eb6668caadda1e2e192ca50c6d2f7a6
Vivek Brohma <vivekb@spotify.com>
2022-09-07 15:33:29 +0200

    added "gmd" comment to delete branches

---
+++ utils/proc.js
@@ -0,0 +1,26 @@
+const { spawn } = require('node:child_process');
+
+/**
+ *
+ * @param {String} command - Command to start
+ * @param {String[] | undefined} args - Arguments fot the command
+ */
+function spawnProc(command, args) {
+  return new Promise((resolve, reject) => {
+    const data = [];
+    const errors = []
+    const process = spawn(command, args);
+    process.stdout.on('data', (chunk) => data.push(chunk.toString('utf-8')));
+
+    try {
+      process.stderr.on('data', (chunk) => errors.push(chunk.toString('utf-8')));
+    } catch (err) {
+      console.error('HMMMM', err)
+    }
+
+    process.on('error', reject);
+    process.on('close', () => errors.length ? reject(errors.join('')) : resolve(data.join('')))
+  });
+}
+
+module.exports = spawnProc;
